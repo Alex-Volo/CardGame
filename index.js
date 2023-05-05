09cdfcbcd6a955aadee5e29a19aa02ed78f1e8f6
Alex Volo <alex_volo@bk.ru>
2023-05-06 01:08:04 +0900

    hw1 done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 "use strict"
+
+function renderApp(mode = "difficulty") {
+    const appElem = document.querySelector('.app-container')
+
+    switch (mode) {
+        case "difficulty": 
+            appElem.innerHTML = `
+                <div class="difficulty">
+                    <h1 class="difficulty__heading">Выбери<br>сложность</h1>
+                    <div class="difficulty__selection">
+                        <button class="difficulty__selection-item">1</button>
+                        <button class="difficulty__selection-item">2</button>
+                        <button class="difficulty__selection-item">3</button>
+                    </div>
+                    <button class="btn start-button">Старт</button>
+                </div>
+            `
+    }
+}
+
+renderApp();
