4a85045859ee5c13abd6e2dd4aef15aa9833d10e
Alex-Volo <122879924+Alex-Volo@users.noreply.github.com>
2023-05-11 21:22:54 +0900

    hw1 done (#1)
    hw1 done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 "use strict"
+localStorage.setItem('CardGame_status', 'difficulty')
+function renderApp(mode = "difficulty") {
+    const appElem = document.querySelector('.app-container')
+
+    switch (mode) {
+        case "difficulty": 
+            appElem.innerHTML = `
+                <div class="difficulty">
+                    <h1 class="difficulty__heading">Выбери<br>сложность</h1>
+                    <div class="difficulty__selection">
+                        <button class="difficulty__selection-item">1</button>
+                        <button class="difficulty__selection-item">2</button>
+                        <button class="difficulty__selection-item">3</button>
+                    </div>
+                    <button class="btn start-button">Старт</button>
+                </div>
+            `
+    }
+    const difficultyButtons = document.querySelectorAll('.difficulty__selection-item');
+    for(let button of difficultyButtons){
+        button.addEventListener('click', () => {
+            difficultyButtons.forEach((el) => el.classList.remove('difficulty__selection-item_checked'));
+            button.classList.add('difficulty__selection-item_checked')
+        })
+    }
+}
+
+renderApp(localStorage.getItem('CardGame_status'));
