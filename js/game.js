eab445951c75761c312fb33e86586250037307b4
Alex Volo <alex_volo@bk.ru>
2023-05-14 21:49:33 +0900

    working with cards style

--- js/game.js
+++ js/game.js
@@ -7,25 +7,56 @@ const suitsBackground = {
 }
 export function renderGameField(difficulty = 1) {
     const deck = new Deck;
-    const gameField = document.querySelector('.game__field');
 
     switch (difficulty) {
         case '1': 
-            deck.shuffle().cut(6).double().shuffle();
-            console.log(deck);
-            for(let card of deck.cards){
-                const cardDiv = document.createElement('div');
-                cardDiv.dataset.value = card.value;
-                cardDiv.dataset.suit = card.suit;
-                cardDiv.classList.add('game__card');
-                cardDiv.style.background = `url(../img/${suitsBackground[card.suit]}) center no-repeat, #FFF`;
-                gameField.append(cardDiv);
-            }
+            deck.shuffle().cut(3).double().shuffle();
+            renderCards(deck);
+            addCardListener()
             break;
         
         case '2':
+            deck.shuffle().cut(6).double().shuffle();
+            renderCards(deck);
+            addCardListener()
             break;
 
         case '3':
+            deck.shuffle().cut(9).double().shuffle();
+            renderCards(deck);
+            addCardListener()
+    }
+}
+
+function renderCards(deck) {
+    const gameField = document.querySelector('.game__field');
+    for(let card of deck.cards){
+        gameField.innerHTML = gameField.innerHTML + `
+        
+        <div data-value=${card.value} data-suit=${card.suit} class="card" >
+            <div class="card__back"></div>
+            <div class="card__face" style="background: url(./img/${suitsBackground[card.suit]}) center center no-repeat, rgb(255, 255, 255);">
+                <div class="card__top">    
+                    <div class="card__value">${card.value}
+                    </div>
+                    <img class="card__suit" src="img/${suitsBackground[card.suit]}" alt="suit">
+                </div>
+                <div class="card__bottom">    
+                    <div class="card__value">${card.value}
+                    </div>
+                    <img class="card__suit" src="img/${suitsBackground[card.suit]}" alt="suit">
+                </div>
+            </div>
+        </div>
+        `
+    }
+}
+function addCardListener() {
+    const cards = document.body.querySelectorAll('.card');
+
+    for (let card of cards) {
+        card.addEventListener('click', () => {
+            console.log('Карта');
+        })
     }
 }
