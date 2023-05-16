a048d5d55eb6865067cbc2adce3bf7dcf47d1761
Alex-Volo <122879924+Alex-Volo@users.noreply.github.com>
2023-05-17 05:45:51 +0900

    Draft (#4)
    * begin work with timer

--- js/deck.js
+++ js/deck.js
@@ -1,41 +1,41 @@
-const SUITS = ['♠', '♣', '♥', '♦']
-const VALUES = ['6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+const SUITS = ['♠', '♣', '♥', '♦'];
+const VALUES = ['6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 const CARDS = VALUES.reduce((result, value) => {
     for (let suit of SUITS) {
         result.push({
             value: value,
             suit: suit,
-        })
+        });
     }
-    return result
-}, [])
+    return result;
+}, []);
 
 export class Deck {
     constructor() {
-        this.cards = CARDS
+        this.cards = CARDS;
     }
 
     shuffle() {
         for (let i = this.cards.length - 1; i > 0; i--) {
-            const newIndex = Math.floor(Math.random() * (i + 1))
+            const newIndex = Math.floor(Math.random() * (i + 1));
             //   const oldValue = this.cards[newIndex]
             //   this.cards[newIndex] = this.cards[i]
             //   this.cards[i] = oldValue
-            ;[this.cards[i], this.cards[newIndex]] = [
+            [this.cards[i], this.cards[newIndex]] = [
                 this.cards[newIndex],
                 this.cards[i],
-            ]
+            ];
         }
-        return this
+        return this;
     }
 
     cut(length = 3) {
-        this.cards = this.cards.slice(0, length)
-        return this
+        this.cards = this.cards.slice(0, length);
+        return this;
     }
 
     double() {
-        this.cards = [this.cards, ...this.cards].flat()
-        return this
+        this.cards = [this.cards, ...this.cards].flat();
+        return this;
     }
 }
