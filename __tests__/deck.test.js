68c45ebea67702112320dce959f4970581f1822d
Alex Volo <alex_volo@bk.ru>
2023-05-27 00:38:11 +0900

    it's alive

--- __tests__/deck.test.js
+++ __tests__/deck.test.js
@@ -1,15 +1,15 @@
 // import * as assert from 'assert';
-const assert = require("assert").strict;
-import { Deck } from '../src/components/deck.js';
+// const assert = require("assert").strict;
+import { Deck } from '../src/components/deck.ts';
+
 test('Deck', () => {
-    
-})
-{
     // Prepare
     const expected = '♠';
     // Action
     const deck = new Deck();
     const result = deck.SUITS[0];
     // Compare
-    assert.equal(result, expected);
-}
+
+    expect(result).toEqual(expected);
+});
+
